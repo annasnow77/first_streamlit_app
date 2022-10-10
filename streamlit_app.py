0312861237f3e012415576189b0bc983b2994a96
anna.snow77 <114983640+annasnow77@users.noreply.github.com>
2022-10-10 08:20:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,28 +28,18 @@ streamlit.dataframe(fruits_to_show)
 
 #create 1 function
 def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
    #take json reponse and normalize it
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
-
-#display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-       streamlit.error("Please select a fruit to get information.")
-  else:
-      back_from_function = get_fruityvice_data(fruit_choice)
-      #output as table
-      streamlit.dataframe(back_from_function)
-      
+   
 except ULRError as e:
   streamlit.error()
 
-streamlit.write('The user entered', fruit_choice)                                     
+streamlit.write('The user entered', fruit_choice)    
+
 #don't run any code from below
-#streamlit.stop()
+streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
@@ -61,6 +51,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
    
 #my_cur = my_cnx.cursor()
